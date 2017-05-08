7a4340032f602cfbdc92676dcc6e737d5d6ec2fd
Nicholas Bishop <nbishop@neverware.com>
2017-05-08 10:49:46 -0400

    Add stubs and conditionals to allow non-cross-compile builds
    Note that this merely allows compiling and linking, it does not make
    gondar run properly.

---
+++ stubs.cc
@@ -0,0 +1,11 @@
+// TODO(nicholasbishop): move the extern C to the header file
+extern "C" {
+#include "gondar.h"
+}
+
+DeviceGuyList* GetDeviceList() {
+  return nullptr;
+}
+
+void Install(DeviceGuy * target_device, char * image_path) {
+}
