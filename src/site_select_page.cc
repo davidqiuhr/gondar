1ae897bd2e9ed60a66ed0e0f1b25d7dee1c73169
Nicholas Bishop <nicholasbishop@gmail.com>
2017-07-21 15:40:35 -0400

    Separate QRadioButton from GondarSite

--- src/site_select_page.cc
+++ src/site_select_page.cc
@@ -1,7 +1,23 @@
 #include "site_select_page.h"
 
+#include <QRadioButton>
+
 #include "gondarsite.h"
 #include "gondarwizard.h"
+
+class SiteButton : public QRadioButton {
+ public:
+  SiteButton(const GondarSite& site) : site_(site) {
+    setText(site.getSiteName());
+  }
+
+  const GondarSite& site() const {
+    return site_;
+  }
+
+ private:
+  const GondarSite site_;
+};
 
 SiteSelectPage::SiteSelectPage(QWidget* parent) : WizardPage(parent) {
   setTitle("Site Select");
@@ -12,9 +28,9 @@ SiteSelectPage::SiteSelectPage(QWidget* parent) : WizardPage(parent) {
 }
 
 void SiteSelectPage::initializePage() {
-  QList<GondarSite*>* sitesList = &wizard()->chromeoverLoginPage.siteList;
-  for (int i = 0; i < sitesList->size(); i++) {
-    GondarSite* curButton = sitesList->at(i);
+  const auto& sitesList = wizard()->chromeoverLoginPage.siteList;
+  for (const auto& site : sitesList) {
+    auto* curButton = new SiteButton(*site);
     sitesButtons.addButton(curButton);
     layout.addWidget(curButton);
   }
@@ -23,17 +39,19 @@ void SiteSelectPage::initializePage() {
 bool SiteSelectPage::validatePage() {
   // if we have a site selected, update our download links and continue
   // otherwise, return false
-  GondarSite* selected =
-      dynamic_cast<GondarSite*>(sitesButtons.checkedButton());
+  auto* selected =
+      dynamic_cast<SiteButton*>(sitesButtons.checkedButton());
   if (selected == NULL) {
     return false;
   } else {
+    const auto site = selected->site();
+
     // if we have a site selected, update our download links for that
     // go find the sitesList
-    qDebug() << "DEBUG: url64 selected=" << selected->get64Url();
-    qDebug() << "DEBUG: url32 selected=" << selected->get32Url();
-    wizard()->imageSelectPage.set64Url(selected->get64Url());
-    wizard()->imageSelectPage.set32Url(selected->get32Url());
+    qDebug() << "DEBUG: url64 selected=" << site.get64Url();
+    qDebug() << "DEBUG: url32 selected=" << site.get32Url();
+    wizard()->imageSelectPage.set64Url(site.get64Url());
+    wizard()->imageSelectPage.set32Url(site.get32Url());
     return true;
   }
 }
