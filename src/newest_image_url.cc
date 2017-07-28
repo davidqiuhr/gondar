5801299a85a153e155652224bdc38b9c8b9559a4
Steven 'Steve' Kendall <skendall@neverware.com>
2017-07-28 13:34:42 -0400

    neglected to add .cc/.h files for newest_image_url

---
+++ src/newest_image_url.cc
@@ -0,0 +1,65 @@
+
+#include "newest_image_url.h"
+
+#include <QNetworkReply>
+#include <QString>
+#include <QUrl>
+#include "log.h"
+
+NewestImageUrl::NewestImageUrl() {
+  url32Ready = false;
+  url64Ready = false;
+}
+
+void NewestImageUrl::fetch() {
+  QString baseUrl(
+      "https://s3.amazonaws.com/neverware-cloudready-free-releases/");
+    // for the free version, we have to find out what the url is
+  connect(&networkManager, &QNetworkAccessManager::finished, this, &NewestImageUrl::handleReply);
+  networkManager.get(QNetworkRequest(QUrl(baseUrl + "latest-stable-64bit")));
+  networkManager.get(QNetworkRequest(QUrl(baseUrl + "latest-stable-32bit")));
+}
+
+void NewestImageUrl::handleReply(QNetworkReply* reply) {
+  const auto error = reply->error();
+  if (error != QNetworkReply::NoError) {
+    LOG_ERROR << "Error retrieving CloudReady Home Edition URL";
+  }
+  // we find out which request this is a response for
+  // TODO: can readAll() be trusted to read our path strings properly?
+  QUrl url = QUrl(QString("https://ddnynf025unax.cloudfront.net/") + reply->readAll());
+  if (reply->url().toString().contains("32bit")) {
+    thirtyTwoUrl = url;
+    url32Ready = true;
+  } else {
+    sixtyFourUrl = url;
+    url64Ready = true;
+  }
+  qDebug() << reply->url();
+  qDebug() << reply->readAll();
+  reply->deleteLater();
+}
+
+void NewestImageUrl::set32Url(QUrl url_in) {
+  thirtyTwoUrl = url_in;
+}
+
+void NewestImageUrl::set64Url(QUrl url_in) {
+  sixtyFourUrl = url_in;
+}
+
+bool NewestImageUrl::isReady() {
+  if (thirtyTwoUrl.isEmpty() || sixtyFourUrl.isEmpty()) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+QUrl NewestImageUrl::get32Url() {
+  return thirtyTwoUrl;
+}
+
+QUrl NewestImageUrl::get64Url() {
+  return sixtyFourUrl;
+}
---
+++ src/newest_image_url.h
@@ -0,0 +1,29 @@
+
+#ifndef NEWEST_IMAGE_URL_H
+#define NEWEST_IMAGE_URL_H
+
+#include <QUrl>
+#include <QNetworkReply>
+
+class NewestImageUrl : public QObject {
+  Q_OBJECT
+
+ public:
+  NewestImageUrl();
+  void fetch();
+  bool isReady();
+  void set32Url(QUrl url_in);
+  void set64Url(QUrl url_in);
+  QUrl get32Url();
+  QUrl get64Url();
+ protected:
+  void handleReply(QNetworkReply* reply);
+ private:
+  QNetworkAccessManager networkManager;
+  bool url32Ready;
+  bool url64Ready;
+  QUrl thirtyTwoUrl;
+  QUrl sixtyFourUrl;
+};
+
+#endif
