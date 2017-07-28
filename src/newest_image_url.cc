399b47f0f544be78bf6f882d0c0fe9ff74c034c0
Steven 'Steve' Kendall <skendall@neverware.com>
2017-07-28 14:15:25 -0400

    remove emit; will pass up errors to wizard so it may navigate to error page

--- src/newest_image_url.cc
+++ src/newest_image_url.cc
@@ -29,7 +29,7 @@ void NewestImageUrl::handleReply(QNetworkReply* reply) {
   const auto error = reply->error();
   if (error != QNetworkReply::NoError) {
     LOG_ERROR << "Error retrieving CloudReady Home Edition URL";
-    emit reply->error();
+    // TODO: propagate error up to wizard
     return;
   }
   // we find out which request this is a response for
