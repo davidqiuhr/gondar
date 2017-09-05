472127ecc710220a1401d57554ec1da4a66cd6aa
Steven 'Steve' Kendall <skendall@neverware.com>
2017-09-05 18:05:23 -0400

    add gondarimage.h

---
+++ src/gondarimage.h
@@ -0,0 +1,31 @@
+
+#ifndef GONDARIMAGE_H
+#define GONDARIMAGE_H
+
+class GondarImage {
+  public:
+    GondarImage() {
+        this->product = QString("UHOH");
+        this->imageName = QString("OHDEAR");
+        this->url = QUrl();
+    }
+    GondarImage(QString productIn, QString imageNameIn, QUrl urlIn) {
+        this->product = productIn;
+        this->imageName = imageNameIn;
+        this->url = urlIn;
+    }
+    QString getCompositeName() {
+        return product + " " + imageName;
+    }
+    bool is32Bit() {
+        return imageName.contains("32-bit", Qt::CaseInsensitive);
+    }
+    bool isDeployable() {
+        return imageName.contains("deployable", Qt::CaseInsensitive);
+    }
+    QString product;
+    QString imageName;
+    QUrl url;
+};
+
+#endif
