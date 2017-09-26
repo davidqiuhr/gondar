ae2e82c0369cbb3e3fe22a808d92bb707aadfc0a
dgs3 <sayles.dave@gmail.com>
2017-09-26 15:44:30 -0400

    Fixed type

--- gondar-release.py
+++ gondar-release.py
@@ -86,7 +86,7 @@ def upload_file(prod_client, local_path, candidate, beerover):
                                  DEST_FILENAME])
   print("uploading to key={}".format(stable_key_path))
   print("archiving release to key={}".format(candidate_key_path))
-  for key in (stable_key_pay, candidate_key_path):
+  for key in (stable_key_path, candidate_key_path):
     prod_client.upload_file(local_path, DEST_BUCKET, key)
     # make the files public
     set_public_read(prod_client, DEST_BUCKET, key)
