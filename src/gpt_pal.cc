ba14db6d13a4acb9de619871a6f604b5e58ade71
Steven 'Steve' Kendall <skendall@neverware.com>
2017-09-28 13:31:10 -0400

    now always use new GPT partition table when making GPTData object
    Addresses OVER-5457

--- src/gpt_pal.cc
+++ src/gpt_pal.cc
@@ -18,10 +18,49 @@
 // We use gdisk to clean up the GPT such that Windows is happy writing to
 // the disk
 #include "../gdisk/gpt.h"
+#include "../gdisk/crc32.h"
+#include <stdio.h>
+
+class PalData : public GPTData {
+public:
+  PalData(string filename);
+  WhichToUse UseWhichPartitions(void) override;
+};
+
+PalData::PalData(string filename) {
+    // if we want to call our own copy of UseWhichPartitions()
+   blockSize = SECTOR_SIZE; // set a default
+   diskSize = 0;
+   partitions = NULL;
+   state = gpt_invalid;
+   device = "";
+   justLooking = 0;
+   mainCrcOk = 0;
+   secondCrcOk = 0;
+   mainPartsCrcOk = 0;
+   secondPartsCrcOk = 0;
+   apmFound = 0;
+   bsdFound = 0;
+   sectorAlignment = MIN_AF_ALIGNMENT; // Align partitions on 4096-byte boundaries by default
+   beQuiet = 0;
+   whichWasUsed = use_new;
+   mainHeader.numParts = 0;
+   numParts = 0;
+   // Initialize CRC functions...
+   chksum_crc32gentab();
+   if (!LoadPartitions(filename))
+      exit(2);
+}
+
+WhichToUse PalData::UseWhichPartitions(void) {
+  // The disk may be in a weird state, but we are about to reformat it.
+  // Just always use a new partition table.
+  return use_gpt;
+}
 
 bool clearMbrGpt(const char* physical_path) {
   std::string physical_path_str(physical_path);
-  GPTData gptdata(physical_path_str);
+  PalData gptdata(physical_path_str);
   // attempt to fix any gpt/mbr problems by setting to a sane, empty state
   gptdata.ClearGPTData();
   gptdata.MakeProtectiveMBR();
