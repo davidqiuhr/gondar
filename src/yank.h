18206fa34f98c77c6ae13f39f4e679be2543f0d1
Steven 'Steve' Kendall <skendall@neverware.com>
2018-09-19 21:24:50 -0400

    WIP

---
+++ src/yank.h
@@ -0,0 +1,18 @@
+
+#ifndef YANK_H_
+#define YANK_H_
+
+#include <windows.h>
+#include <windowsx.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <shlobj.h>
+#include <commdlg.h>
+#include <richedit.h>
+
+#include "msapi_utf8_2.h"
+
+BOOL SetFormatPromptHook(void);
+
+#endif /* YANK_H_ */
