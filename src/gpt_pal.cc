62ba9e7d407980cff2fab06f49ff1a882c910116
Steven 'Steve' Kendall <skendall@neverware.com>
2018-09-25 17:25:45 -0400

    progress

--- src/gpt_pal.cc
+++ src/gpt_pal.cc
@@ -15,6 +15,12 @@
 
 #include "gpt_pal.h"
 
+#define PF_INIT(proc, name)         if (pf##proc == NULL) pf##proc = \
+  (proc##_t) GetProcAddress(GetLibraryHandle(#name), #proc)
+#define PF_INIT_OR_OUT(proc, name)      do {PF_INIT(proc, name);         \
+  if (pf##proc == NULL) {uprintf("Unable to locate %s() in %s.dll: %s\n",  \
+  #proc, #name, ""); goto out;} } while(0)
+
 // We use gdisk to clean up the GPT such that Windows is happy writing to
 // the disk
 #include "../gdisk/gpt.h"
@@ -24,6 +30,62 @@
 
 // FIXME: temp
 #include <stdio.h>
+
+/* Callback command types (some errorcode were filled from HPUSBFW V2.2.3 and their
+   designation from msdn.microsoft.com/en-us/library/windows/desktop/aa819439.aspx */
+typedef enum {
+  FCC_PROGRESS,
+  FCC_DONE_WITH_STRUCTURE,
+  FCC_UNKNOWN2,
+  FCC_INCOMPATIBLE_FILE_SYSTEM,
+  FCC_UNKNOWN4,
+  FCC_UNKNOWN5,
+  FCC_ACCESS_DENIED,
+  FCC_MEDIA_WRITE_PROTECTED,
+  FCC_VOLUME_IN_USE,
+  FCC_CANT_QUICK_FORMAT,
+  FCC_UNKNOWNA,
+  FCC_DONE,
+  FCC_BAD_LABEL,
+  FCC_UNKNOWND,
+  FCC_OUTPUT,
+  FCC_STRUCTURE_PROGRESS,
+  FCC_CLUSTER_SIZE_TOO_SMALL,
+  FCC_CLUSTER_SIZE_TOO_BIG,
+  FCC_VOLUME_TOO_SMALL,
+  FCC_VOLUME_TOO_BIG,
+  FCC_NO_MEDIA_IN_DRIVE,
+  FCC_UNKNOWN15,
+  FCC_UNKNOWN16,
+  FCC_UNKNOWN17,
+  FCC_DEVICE_NOT_READY,
+  FCC_CHECKDISK_PROGRESS,
+  FCC_UNKNOWN1A,
+  FCC_UNKNOWN1B,
+  FCC_UNKNOWN1C,
+  FCC_UNKNOWN1D,
+  FCC_UNKNOWN1E,
+  FCC_UNKNOWN1F,
+  FCC_READ_ONLY_MODE,
+} FILE_SYSTEM_CALLBACK_COMMAND;
+
+typedef BOOLEAN (__stdcall *FILE_SYSTEM_CALLBACK)(
+  FILE_SYSTEM_CALLBACK_COMMAND Command,
+  ULONG                        Action,
+  PVOID                        pData
+);
+
+/* Parameter names aligned to
+   http://msdn.microsoft.com/en-us/library/windows/desktop/aa819439.aspx */
+typedef VOID (WINAPI *FormatEx_t)(
+  WCHAR*               DriveRoot,
+  MEDIA_TYPE           MediaType,   // See WinIoCtl.h
+  WCHAR*               FileSystemTypeName,
+  WCHAR*               Label,
+  BOOL                 QuickFormat,
+  ULONG                DesiredUnitAllocationSize,
+  FILE_SYSTEM_CALLBACK Callback
+);
 
 class PalData : public GPTData {
  public:
@@ -102,11 +164,19 @@ bool makeEmptyPartition(const char* physical_path) {
   // make an unformatted partition with label for fat32
   gptdata.ClearDisk();
   // now make the volume
-  //pfFormatEx(wVolumeName, DRIVE_REMOVABLE, &wFSType[index], wLabel,
-  //  IsChecked(IDC_QUICK_FORMAT), ulClusterSize, FormatExCallback);
+
+  FormatEx_t pfFormatEx = NULL;
+  // LoadLibrary("fmifs.dll") appears to changes the locale, which can lead to
+  // problems with tolower(). Make sure we restore the locale. For more details,
+  // see http://comments.gmane.org/gmane.comp.gnu.mingw.user/39300
+  char* locale = setlocale(LC_ALL, NULL);
+  PF_INIT_OR_OUT(FormatEx, Fmifs);
+  PF_INIT(EnableVolumeCompression, Fmifs);
+  setlocale(LC_ALL, locale);
+
   // 0x0b is RemovableMedia according to https://msdn.microsoft.com/en-us/library/windows/desktop/aa365231(v=vs.85).aspx
-  // TODO: lettuce use the actual value
   pfFormatEx(physical_path, MEDIA_TYPE.RemovableMedia, L"FAT32", L"", /*quick*/true, /*clustersize*/512, kewlcallback);
+
   int problems = gptdata.Verify();
   free(newPartInfo);
   // TODO: unclear if we care about problems in this regard
