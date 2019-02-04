74d01abd37e67ccd978cdbdc26a2425c8bb513e7
Steven 'Steve' Kendall <skendall@neverware.com>
2019-02-04 15:07:13 -0500

    now return non-zero if linting problems are found for PR builder

--- infra/lint.py
+++ infra/lint.py
@@ -36,9 +36,8 @@ def main():
         try:
             subprocess.check_call(cmd)
         except subprocess.CalledProcessError:
-            # TODO: eventually raise this once it is integrated into PR builder
-            # For now there are quite a few errors
-            print('Errors found')
+            print('Lint problems found')
+            raise
 
 
 if __name__ == '__main__':
