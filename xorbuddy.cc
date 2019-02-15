300a414e8d1c49f197fc1298b976ee06b6ef9571
Steven 'Steve' Kendall <skendall@neverware.com>
2019-02-15 18:50:16 -0500

    this is more like the logic i want

--- xorbuddy.cc
+++ xorbuddy.cc
@@ -1,20 +1,39 @@
 
 #include <QByteArray>
 #include <iostream>
+#include <random>
 
-int main(int argc, char ** argv) {
-  QByteArray input("my dog has fleas");
-  unsigned int rando = 92;
-  QByteArray stored;
-  for (QByteArray::iterator it = input.begin(); it != input.end(); it++) {
-    stored.append(it[0]^rando);
+QByteArray getRand(int len) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, 255);
+  QByteArray output;
+  for (int i = 0; i < len; i++) {
+    unsigned char cur = dis(gen);
+    std::cout << "cur = " << (unsigned int)cur << std::endl;
+    output.append(cur);
+  }
+  bool ok;
+  return output;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("wrong number of args\n");
+    return 0;
+  }
+  QByteArray input(argv[1]);
+  QByteArray salt = getRand(input.length());
+  QByteArray derived;
+  for (int i = 0 ; i < input.length(); i++) {
+    derived.append(input.at(i)^salt.at(i));
   }
   // then we store our data...
 
   // and extract it later:
   QByteArray output;
-  for (QByteArray::iterator it = stored.begin(); it != stored.end(); it++) {
-    output.append(it[0]^rando);
+  for (int i = 0; i < derived.length(); i++) {
+    output.append(derived.at(i)^salt.at(i));
   }
-  std::cout << output.toStdString() << std::endl;
+  std::cout << "final output: " << output.toStdString() << std::endl;
 }
