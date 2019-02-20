ee8610dd63bcb6572b36838132ef71485956a03f
Steven 'Steve' Kendall <skendall@neverware.com>
2019-02-20 17:00:57 -0500

    lint/format

--- src/util.cc
+++ src/util.cc
@@ -39,9 +39,10 @@ QByteArray getGoogleSignInId() {
 
 // get the google sign in client secret
 QByteArray getGoogleSignInSecret() {
-// TODO: ifdef on both hash1 and 2
+// TODO(kendall): ifdef on both hash1 and 2
 #ifdef GOOGLE_SIGN_IN_SECRET_HASH1
-  QByteArray client_secret = get_string(GOOGLE_SIGN_IN_SECRET_HASH1, GOOGLE_SIGN_IN_SECRET_HASH2);
+  QByteArray client_secret =
+      get_string(GOOGLE_SIGN_IN_SECRET_HASH1, GOOGLE_SIGN_IN_SECRET_HASH2);
   return client_secret.toLatin1();
 #else
   return QByteArray();
--- src/xor.h
+++ src/xor.h
@@ -1,6 +1,25 @@
+// Copyright 2019 Neverware
+//
+// The source code in this file was derived from:
+//
+//     Rufus: The Reliable USB Formatting Utility
+//     Copyright © 2011-2016 Pete Batard <pete@akeo.ie>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef _XOR_H_
-#define _XOR_H_
+#ifndef SRC_XOR_H_
+#define SRC_XOR_H_
 
 #include <QByteArray>
 #include <QString>
@@ -11,4 +30,4 @@ QByteArray getByteArrayFromString(QString in);
 QByteArray get_hash(QString in, QByteArray hash1);
 QString get_string(QString hash1str, QString hash2str);
 
-#endif  //  _XOR_H_
+#endif  //  SRC_XOR_H_
