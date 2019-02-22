ee4f94dbb815ea7fdfb817be94c1d4d1c0d7d0c8
Steven 'Steve' Kendall <skendall@neverware.com>
2019-02-22 16:56:34 -0500

    still broken

--- src/randar.cc
+++ src/randar.cc
@@ -3,16 +3,14 @@
 
 #if defined(Q_OS_WIN)
 #include <windows.h>
-#include <inttypes.h>
-#include <setupapi.h>
 #undef WIN32_NO_STATUS
 
-#include <winternl.h>
-#include <ntstatus.h>
-#include <winerror.h>
-#include <stdio.h>
 #include <bcrypt.h>
+#include <ntstatus.h>
 #include <sal.h>
+#include <stdio.h>
+#include <winerror.h>
+#include <winternl.h>
 #endif
 
 #include <random>
@@ -29,22 +27,18 @@ RandomManager::RandomManager() {
 
 #if defined(Q_OS_WIN)
 unsigned int RandomManager::getSeed() {
-    BYTE    Buffer[4];
-    DWORD   BufferSize;
-    
-    BufferSize = sizeof (Buffer);
-    memset (Buffer, 0, BufferSize);
-    
-    //
-    // Fill the buffer with random bytes
-    //
+  BYTE Buffer[4];
+  DWORD BufferSize;
 
-    BCryptGenRandom(NULL,  // Alg Handle pointer; NUll is passed as BCRYPT_USE_SYSTEM_PREFERRED_RNG flag is used
-                    Buffer,                     // Address of the buffer that recieves the random number(s)
-                    BufferSize,                 // Size of the buffer in bytes
-                    BCRYPT_USE_SYSTEM_PREFERRED_RNG); // Flags                  
-    int ret = (Buffer[3] << 24) | (Buffer[2] << 16) | (Buffer[1] << 8) | (Buffer[0]);
-    return ret;
+  BufferSize = sizeof(Buffer);
+  memset(Buffer, 0, BufferSize);
+
+  // fills buffer with random bytes
+  BCryptGenRandom(NULL, Buffer, BufferSize,
+                  BCRYPT_USE_SYSTEM_PREFERRED_RNG);  // Flags
+  int ret =
+      (Buffer[3] << 24) | (Buffer[2] << 16) | (Buffer[1] << 8) | (Buffer[0]);
+  return ret;
 }
 #else
 
@@ -66,4 +60,4 @@ int getRandomNum(int lower, int higher) {
   LOG_INFO << "generated " << result;
   return result;
 }
-} // namespace gondar
+}  // namespace gondar
--- src/randar.h
+++ src/randar.h
@@ -10,23 +10,23 @@ class RandomManager {
  public:
   static RandomManager& getInstance() {
     static RandomManager instance;
-    return instance; 
+    return instance;
   }
   std::mt19937 gen;
+
  private:
   RandomManager();
   unsigned int getSeed();
   unsigned int seed;
+
  public:
   // singleton buddies
   // see https://stackoverflow.com/questions/1008019/c-singleton-design-pattern
   RandomManager(RandomManager const&) = delete;
   void operator=(RandomManager const&) = delete;
-
 };
 
-int getRandomNum(int lower, int higher); 
-
+int getRandomNum(int lower, int higher);
 }
 
-#endif // SRC_RANDAR_H
+#endif  // SRC_RANDAR_H
