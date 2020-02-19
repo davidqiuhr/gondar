7c6b73574760f903d89c2ab3f77194928ca7987a
Steven 'Steve' Kendall <skendall@neverware.com>
2020-02-19 15:23:44 -0500

    need to linkify this and remove https in text

--- src/meepo.h
+++ src/meepo.h
@@ -42,7 +42,7 @@ class Meepo : public QObject {
 
   QString error() const;
   Sites sites() const;
-  const QString no_sites_error = "User has no sites";
+  const QString no_sites_error = "User has no sites.\nPlease visit https://try.neverware.com to sign up for a trial";
 
  signals:
   void finished();
