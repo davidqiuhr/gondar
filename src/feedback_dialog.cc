351be8293b1fad8665b248eaeeaa040c0c6359b6
Steven 'Steve' Kendall <skendall@neverware.com>
2020-02-24 14:59:01 -0500

    does not compile like this, but generally this idea

---
+++ src/feedback_dialog.cc
@@ -0,0 +1,51 @@
+// Copyright 2017 Neverware
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "feedback_dialog.h"
+
+#include <stdexcept>
+
+#include <QFile>
+
+#include "util.h"
+
+namespace gondar {
+
+FeedbackDialog::FeedbackDialog() {
+  about_label_.setOpenExternalLinks(true);
+  about_label_.setWordWrap(true);
+  about_label_.setText(readUtf8File(":/about.html"));
+
+  license_text_browser_.setOpenExternalLinks(true);
+  license_text_browser_.setHtml(readUtf8File(":/gpl-3.0-standalone.html"));
+  license_text_browser_.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+  close_button_.setText(tr("&Close"));
+  connect(&close_button_, &QPushButton::clicked, this, &FeedbackDialog::accept);
+
+  QString version = gondar::getGondarVersion();
+  // if version is non-empty
+  if (version.size() > 0) {
+    version_label_.setText("Version " + version);
+    layout_.addWidget(&version_label_);
+  }
+  layout_.addWidget(&about_label_);
+  layout_.addWidget(&license_text_browser_);
+  layout_.addWidget(&close_button_, 0, Qt::AlignRight);
+  setLayout(&layout_);
+  setMinimumWidth(500);
+  setWindowTitle(tr("CloudReady USB Maker"));
+}
+}  // namespace gondar
---
+++ src/feedback_dialog.h
@@ -0,0 +1,42 @@
+// Copyright 2017 Neverware
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SRC_FEEDBACK_DIALOG_H_
+#define SRC_FEEDBACK_DIALOG_H_
+
+#include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+#include <QTextBrowser>
+#include <QVBoxLayout>
+
+namespace gondar {
+
+class FeedbackDialog : public QDialog {
+  Q_OBJECT
+
+ public:
+  FeedbackDialog();
+
+ private:
+  QVBoxLayout layout_;
+  QLabel version_label_;
+  QLabel about_label_;
+  QTextBrowser license_text_browser_;
+  QPushButton close_button_;
+};
+}  // namespace gondar
+
+#endif  // SRC_FEEDBACK_DIALOG_H_
